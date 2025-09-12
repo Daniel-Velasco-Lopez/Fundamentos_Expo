87ca902bb07e91b89e2a96c8c8e5175bed29192e
Daniel Velasco López <danyinfo158@gmail.com>
2025-09-11 20:52:54 -0600

    Creando HomePrincipal

---
+++ src/pages/pantalla_inicio/HomePrincipal.tsx
@@ -0,0 +1,38 @@
+import { Platform, ScrollView, StatusBar, StyleSheet, Text, View } from 'react-native';
+import CardBasico from '../../components/CardBasico';
+import CardImagen from '../../components/CardImagen';
+import CardV2 from '../../components/CardV2';
+import CardV3 from '../../components/CardV3';
+
+
+export default function HomePrincipal() {
+  
+  return (
+    
+    <ScrollView>
+   <View style={styles.mainS}>  
+        <Text style={styles.title}>Pagina Principal</Text>
+     <CardBasico/>
+     <CardImagen/>
+     <CardV2/>
+     <CardV3/>
+    </View>
+    </ScrollView>
+ 
+  );
+}
+const styles = StyleSheet.create({
+  mainS: {
+    flex: 1,
+    backgroundColor: '#91d9f8ff',
+    paddingTop:Platform.OS === 'android' ?  StatusBar.currentHeight:44, 
+    padding:16
+  },
+  title:{
+    fontSize:22,
+    fontWeight:'bold',
+    textAlign:'center',
+    marginTop:16
+  },
+
+});
