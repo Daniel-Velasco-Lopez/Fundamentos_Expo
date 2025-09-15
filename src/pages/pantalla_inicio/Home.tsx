f201eb24f21ef1c4186705f08b25fd3a0fe42a8a
Daniel Velasco López <danyinfo158@gmail.com>
2025-09-14 21:17:29 -0600

    marginTop: 40 de Home

--- src/pages/pantalla_inicio/Home.tsx
+++ src/pages/pantalla_inicio/Home.tsx
@@ -1,32 +1,38 @@
-import { Image, Platform, SafeAreaView, StatusBar, StyleSheet, Text } from 'react-native';
+import { Image, Platform, SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
 import Botones from '../Tipos_botones/Botones';
 
 export default function Home() {
   return (
     <SafeAreaView style={styles.container}>
-      <Image
-        style={styles.image}
-        source={{ uri: 'https://img.freepik.com/foto-gratis/fotografia-completa-adulto-montando-motocicleta-genial_23-2150868296.jpg' }}
-      />
-      <Text style={styles.h1}>Bienvenidos A esta app</Text>
-      <Text style={styles.h2}>Aplicaciones Móviles</Text>
-      <Botones />
+      <View style={styles.contentWrapper}>
+        <Image
+          style={styles.image}
+          source={{ uri: 'https://img.freepik.com/foto-gratis/fotografia-completa-adulto-montando-motocicleta-genial_23-2150868296.jpg' }}
+        />
+        <Text style={styles.h1}>Bienvenidos A esta app</Text>
+        <Text style={styles.h2}>Aplicaciones Móviles</Text>
+        <Botones />
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-  flex: 1,
-  alignItems: 'center',
-  backgroundColor: '#91d9f8ff',
-  paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight || 20 : 60,
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#91d9f8ff',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight || 20 : 60,
+  },
+  contentWrapper: {
+    marginTop: 40, 
+    alignItems: 'center',
   },
   image: {
     width: 150,
     height: 150,
     borderRadius: 75,
-    marginBottom: 20, // separa la imagen del texto
+    marginBottom: 20,
   },
   h1: {
     fontSize: 20,
