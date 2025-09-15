98e4adf716f39554b157ffc82405fa9cb2b1ce0e
Daniel Velasco López <danyinfo158@gmail.com>
2025-09-14 22:27:40 -0600

    App.js cambio de orden

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
 //import Formulario from './src/pages/Formularios/Formulario';
 //import FormularioV2 from './src/pages/Formularios/FormularioV2';
 //import Home from 'C:/Users/CICEM/Fundamentos_Expo/src/pages/pantalla_inicio/Home';
-//import HomePrincipal from 'C:/Users/CICEM/Fundamentos_Expo/src/pages/pantalla_inicio/HomePrincipal';
+import HomePrincipal from 'C:/Users/CICEM/Fundamentos_Expo/src/pages/pantalla_inicio/HomePrincipal';
 //import Botones from './src/pages/Tipos_botones/Botones';
- 
-import Lista_Alumnos from './src/pages/Lista_Alumnos/ListaAlumnos';
+//import Lista_Alumnos from './src/pages/Lista_Alumnos/ListaAlumnos';
 export default function App() {
   return ( 
 
   //<Home />
   //<Botones /> 
   //<BotonesV2 />
-  //<HomePrincipal/> 
+  <HomePrincipal/> 
   //<Formulario/>
   //<FormularioV2/>
-   <Lista_Alumnos/>
+  //<Lista_Alumnos/>
 
   );
 }
