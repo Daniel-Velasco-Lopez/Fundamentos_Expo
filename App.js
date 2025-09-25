7f1411c89d8203dc6be52bedd7a4a7cabbac0791
Daniel Velasco López <danyinfo158@gmail.com>
2025-09-25 00:23:55 -0600

    Corrigiendo detalles en App.js

--- App.js
+++ App.js
@@ -1,3 +1,10 @@
-// App.js
-import 'expo-router/entry';
 
+import { registerRootComponent } from 'expo';
+import { ExpoRoot } from 'expo-router';
+
+// Must be exported or Fast Refresh won't update the context
+export function App() {
+  return <ExpoRoot context={require.context('./app')} />;
+}
+
+registerRootComponent(App); 
